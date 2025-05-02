b53a7f62ef185cf95f1c6240421d72ac272e2ded
Talha Waheed <talhawaheed57@gmail.com>
2025-05-01 21:55:02 -0500

    Update setup-py.sh

--- setup-py.sh
+++ setup-py.sh
@@ -1,4 +1,7 @@
 sudo apt update
 sudo apt install -y python3-pip
+sudo apt install -y python3.12-venv
+python3 -m venv ml-env
+source ml-env/bin/activate
 pip install torch torchvision torchaudio
 pip install matplotlib seaborn
